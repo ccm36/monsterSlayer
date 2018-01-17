4d6f532d5515bb7f373cf21a66cdc7680a39da29
ccm36 <cameron.c.malloy@gmail.com>
2018-01-17 13:21:16 -0500

    Refactor attack method

--- app.js
+++ app.js
@@ -12,27 +12,13 @@ new Vue({
       this.monsterHealth = 100;
     },
     attack: function() {
-      var max = 10;
-      var min = 3;
-      var damage = Math.max(Math.floor(Math.random() + max) + 1, min);
-      this.monsterHealth -= damage;
-
-      if (this.monsterHealth <= 0) {
-        alert('You slayed the monster!');
-        this.gameIsRunning = false;
+      this.monsterHealth -= this.calculateDamage(3, 10);
+      if (this.checkWin()) {
         return;
       }
 
-      max = 12;
-      min = 5;
-      damage = Math.max(Math.floor(Math.random() + max) + 1, min);
-      this.playerHealth -= damage;
-
-      if (this.playerHealth <= 0) {
-        alert('The monster killed you!');
-        this.gameIsRunning = false;
-        return;
-      }
+      this.playerHealth -= this.calculateDamage(5, 12);
+      this.checkWin();
     },
     specialAttack: function() {
 
@@ -42,6 +28,27 @@ new Vue({
     },
     giveUp: function() {
 
+    },
+    calculateDamage: function(min, max) {
+      return Math.max(Math.floor(Math.random() + max) + 1, min);
+    },
+    checkWin: function() {
+      if (this.monsterHealth <= 0) {
+        if (confirm('You slayed the monster! Want to play again?')) {
+          this.startGame();
+        } else {
+          this.gameIsRunning = false;
+        }
+        return true;
+      } else if (this.playerHealth <= 0) {
+        if (confirm('The monster killed you! Want to play again?')) {
+          this.startGame();
+        } else {
+          this.gameIsRunning = false;
+        }
+        return true;
+      }
+      return false;
     },
   }
 });
