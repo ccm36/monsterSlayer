a5ed70152024db3d791653e86fb234271ccee898
ccm36 <cameron.c.malloy@gmail.com>
2018-01-17 13:07:03 -0500

    Add attack method (TODO: refactor)

--- app.js
+++ app.js
@@ -12,7 +12,27 @@ new Vue({
       this.monsterHealth = 100;
     },
     attack: function() {
-      
+      var max = 10;
+      var min = 3;
+      var damage = Math.max(Math.floor(Math.random() + max) + 1, min);
+      this.monsterHealth -= damage;
+
+      if (this.monsterHealth <= 0) {
+        alert('You slayed the monster!');
+        this.gameIsRunning = false;
+        return;
+      }
+
+      max = 12;
+      min = 5;
+      damage = Math.max(Math.floor(Math.random() + max) + 1, min);
+      this.playerHealth -= damage;
+
+      if (this.playerHealth <= 0) {
+        alert('The monster killed you!');
+        this.gameIsRunning = false;
+        return;
+      }
     },
     specialAttack: function() {
 
@@ -23,4 +43,9 @@ new Vue({
     giveUp: function() {
 
     },
+  }
 });
+
+
+
+
